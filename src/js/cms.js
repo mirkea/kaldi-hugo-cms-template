13e3c04d9496e6d2f08b0948086a1a97b6ed79ca
Unknown <mircea@code11.com>
2018-01-04 14:05:41 +0200

    added demo widgets

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,6 +3,9 @@ import CMS from "netlify-cms";
 
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
+import { SlidesControl, SlidesPreview } from "./Slides";
+import { Slides2Control, Slides2Preview } from "./Slides2";
+// import { StringzControl, StringzPreview } from "./Stringz";
 
 
 // Example of creating a custom color widget
@@ -21,3 +24,7 @@ CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerWidget("color", ColorControl);
+CMS.registerWidget("slides", SlidesControl, SlidesPreview);
+CMS.registerWidget("slides2", Slides2Control, Slides2Preview);
+// CMS.registerWidget("stringz", StringzControl, StringzPreview);
+
