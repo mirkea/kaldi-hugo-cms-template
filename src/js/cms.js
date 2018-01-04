e682fff4f85539773f49d65735ba6c7f502788a1
Unknown <mircea@code11.com>
2018-01-04 16:53:37 +0200

    addec categories widget

--- src/js/cms.js
+++ src/js/cms.js
@@ -4,7 +4,8 @@ import CMS from "netlify-cms";
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
 import { SlidesControl, SlidesPreview } from "./Slides";
-import { Slides2Control, Slides2Preview } from "./Slides2";
+// import { TranslationsControl, TranslationsPreview } from "./Translations";
+// import { Slides2Control, Slides2Preview } from "./Slides2";
 // import { StringzControl, StringzPreview } from "./Stringz";
 
 
@@ -20,11 +21,47 @@ class ColorControl extends React.Component {
   }
 }
 
+var CategoriesControl = createClass({
+  handleChange: function(e) {
+    this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
+  },
+
+  render: function() {
+    var value = this.props.value;
+    return  <div className='ioio' >{h('input', { type: 'text', value: value ? value.join(', ') : '', onChange: this.handleChange })} </div>;
+  }
+});
+
+var CategoriesPreview = createClass({
+  render: function() {
+    return h('ul', {},
+      this.props.value.map(function(val, index) {
+        return h('li', {key: index}, val);
+      })
+    );
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+CMS.registerWidget('categories', CategoriesControl, CategoriesPreview);
+// CMS.registerWidget('translations', TranslationsControl, TranslationsPreview);
+
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerWidget("color", ColorControl);
 CMS.registerWidget("slides", SlidesControl, SlidesPreview);
-CMS.registerWidget("slides2", Slides2Control, Slides2Preview);
+// CMS.registerWidget("slides2", Slides2Control, Slides2Preview);
 // CMS.registerWidget("stringz", StringzControl, StringzPreview);
 
